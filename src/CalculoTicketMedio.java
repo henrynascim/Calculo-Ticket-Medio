be59e07712c5aac7c2682ad177d05ac7635e8b6b
Henrique Nascimento <114836223+Henryborg@users.noreply.github.com>
2023-03-22 21:52:40 -0300

    finished

---
+++ src/CalculoTicketMedio.java
@@ -0,0 +1,14 @@
+public class CalculoTicketMedio {
+
+    public static void main(String[] args) {
+        int venda1 = 20;
+        int venda2 = 30;
+        int venda3 = 100;
+
+        int ticketMedio = (venda1 + venda2 + venda3) / 3;
+
+        System.out.println("Ticket Medio:" + ticketMedio);
+
+    }
+
+}
